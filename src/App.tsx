b1b22b890cb5aab8d002a1058955a32854d52313
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-03-24 22:22:03 +0100

    import Notes interface from note model

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import { Notes } from "./models/note.models";
+import "./App.css";
 
 function App() {
+  const [note, setNotes] = useState<Notes[]>([{
+    id: (new Date).toString(),
+    title: "Meetings",
+    text: "Schedule a meeting",
+    color: '#dfdfdf',
+    date: (new Date).toString()
+  }]);
   return (
     <div className="App">
       <h1>NoteApp</h1>
